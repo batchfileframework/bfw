47bcdc8dad885560e5fac71776318752821af6c3
batchfileframework <135866314+batchfileframework@users.noreply.github.com>
2024-03-26 07:01:58 -0400

    Add files via upload

--- bfw/bfw.bat
+++ bfw/bfw.bat
@@ -34,7 +34,7 @@ REM ---- Custom functions
 ::Usage Call :CreateBFWlink FunctionName OutputFolder
 :CreateBFWlink
 set "_CreateBFWlink_fileorigin=%~dpnx0"
-if "[%~1]" EQU "[CreateBFWlink]" shift
+if /i "[%~1]" EQU "[CreateBFWlink]" shift
 set "_CreateBFWlink_Output=%~2"
 if not defined _CreateBFWlink_Output set "_CreateBFWlink_Output=%~dp0"
 echo echo Call :CreateLink "%_CreateBFWlink_fileorigin%" "%~1.bat"
