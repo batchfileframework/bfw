18f34eb3cb1f2ca2dce98a9954cd1098b262b55e
batchfileframework <135866314+batchfileframework@users.noreply.github.com>
2024-03-26 05:45:08 -0400

    Create FunctionSwitcherBasic.bat

---
+++ lib/core/FunctionSwitcherBasic.bat
@@ -0,0 +1,13 @@
+@echo off
+
+:setup
+:main
+
+for %%a in ( %* ) do ( for %%b in ( /h /? -h -? help --help ) do ( if "[%%a]" EQU "[%%b]" ( Call :%~n0-help & exit /b 1 ) ) )
+for %%a in ( %* ) do ( if "[%%a]" EQU "[demo]" ( Call :%~n0-demo & exit /b 1 ) ) 
+if "[%~1]" EQU "[]" ( echo %~n0 needs at least one argument & exit /b 1 )
+REM if "[%~1]" EQU "[]" if "[%~2]" EQU "[]" ( echo %~n0 needs at least two argument & exit /b 1 )
+Call :%~n0 %*
+
+:end
+GoTo :EOF
