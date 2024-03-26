669339c22b8b1abd1489c245444392d2a8ca34a4
batchfileframework <135866314+batchfileframework@users.noreply.github.com>
2024-03-26 06:21:31 -0400

    Add files via upload

--- bfw/bfw.bat
+++ bfw/bfw.bat
@@ -7,7 +7,8 @@ for %%a in ( %* ) do ( for %%b in ( /h /? -h -? help --help ) do ( if "[%%a]" EQ
 for %%a in ( %* ) do ( if "[%%a]" EQU "[demo]" ( Call :%~n0-demo & exit /b 1 ) ) 
 if "[%~1]" EQU "[]" ( echo %~n0 needs at least one argument & exit /b 1 )
 REM if "[%~1]" EQU "[]" if "[%~2]" EQU "[]" ( echo %~n0 needs at least two argument & exit /b 1 )
-Call :%~n0 %*
+
+if "[%~n0]" EQU "[bfw]" ( Call :%~1 %* ) else ( Call :%~n0 %* )
 
 GoTo :EOF
 
