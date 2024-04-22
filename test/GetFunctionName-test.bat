4a877b110b5c1f29c02f6ec165fa84cc2cb54e53
batchfileframework <135866314+batchfileframework@users.noreply.github.com>
2024-04-22 05:20:32 -0400

    Add files via upload

--- test/GetFunctionName-test.bat
+++ test/GetFunctionName-test.bat
@@ -20,7 +20,8 @@ GoTo :EOF
 
 ::Usage Call :GetFunctionName File LineNumber OutputValue
 :GetFunctionName
-for /F "usebackq eol= tokens=1,2 delims=(&:=+" %%i in (`^(type %~1 ^| findstr /n /r /c:".*" ^| findstr /B /C:"%~2:" ^) 2^>nul`) do ( set "%~3=%%j" & exit /b 0 )
+for /F "usebackq eol= tokens=1,2 delims=(&:=+ " %%i in (`^(type %~1 ^| findstr /n /r /c:".*" ^| findstr /B /C:"%~2:" ^) 2^>nul`) do ( set "%~3=%%j" & exit /b 0 )
+REM proposed alternative for /F "tokens=2 delims=(&:=+ " %%i in ('%SystemRoot%\System32\findstr.exe /n "" "%~1" ^| %SystemRoot%\System32\findstr.exe /B /C:"%~2:"') do set "%~3=%%i" & exit /b 0
 exit /b 1
 
 ::Usage Call :EchoLineSimple textfilename 3 4 5 6 ... N
